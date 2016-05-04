b2c8e5edcb88d402fca68b8661959a7f2bb3fda5
jennytlee <jennytoralee@gmail.com>
2016-05-04 15:05:40 -0700

    Initial Commit

---
+++ Coat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Coat
+//
+//  Created by Jenny Lee on 5/4/16.
+//  Copyright © 2016 Jenny Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
